a0055e40af6e009421f7b2acbea613f3f2ad9227
Jeffrey Chew <jeff.chew@gmail.com>
2019-11-12 13:30:32 -0500

    Update Footer.js

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -4,33 +4,29 @@ import Footer from 'gatsby-theme-carbon/src/components/Footer';
 const Content = () => (
   <>
     <p>
-      By importing the <strong>Footer</strong> component from
-      gatsby-theme-carbon, we can supply our own props.
+      Have questions? Open an issue in [Github](https://github.com/carbon-design-system/carbon-for-ibm-website/issues/new/choose)
     </p>
     <p>
-      The default export from a shadowed component will replace that component
-      in the theme.
-    </p>
-    <p>
-      <a href="https://www.gatsbyjs.org/docs/themes/api-reference/#component-shadowing">
-        More about component shadowing
-      </a>
+      Last updated November 12th, 2019
+      <br/>
+      Copyright &copy; 2019 IBM
     </p>
   </>
 );
 
 const links = {
   firstCol: [
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
+    { href: 'https://ibm.com/design', linkText: 'Contribute' },
+    { href: 'https://ibm.com/design', linkText: 'Privacy' },
+    { href: 'https://ibm.com/design', linkText: 'Terms of use' },
+    { href: 'https://ibm.com', linkText: 'IBM.com' },
   ],
-  secondCol: [
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
-  ],
+//  secondCol: [
+//    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
+//    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
+//    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
+//    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
+//  ],
 };
 
 const CustomFooter = () => <Footer links={links} Content={Content} />;
