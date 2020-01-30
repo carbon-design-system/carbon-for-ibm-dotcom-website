57679bff69f8aa1af55361f7d4930254e5652af4
Jeff Chew <jeff.chew@gmail.com>
2020-01-30 10:50:40 -0500

    fix(cookiepreferences): adding link element in footer for trustarc

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -1,34 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from 'gatsby-theme-carbon/src/components/Footer';
 
-const Content = () => (
-  <>
-    <p>
-      Have questions? Open an issue in <a href="https://github.com/carbon-design-system/ibm-dotcom-library-website/issues/new/choose">Github</a>.
-    </p>
-    <p>
-      Last updated December 31st, 2019
-      <br/>
-      Copyright &copy; 2019 IBM
-    </p>
-  </>
-);
+/**
+ * Custom Footer which wraps the Gatsby theme Footer
+ * @returns {*} JSX component for the Footer
+ */
+const CustomFooter = () => {
+  /**
+   * Content for the Footer component
+   * @returns {*} Content JSX
+   */
+  const Content = () => (
+    <>
+      <p>
+        Have questions? Open an issue in <a href="https://github.com/carbon-design-system/ibm-dotcom-library-website/issues/new/choose">Github</a>.
+      </p>
+      <p>
+        Last updated December 31st, 2019
+        <br/>
+        Copyright &copy; 2019 IBM
+      </p>
+    </>
+  );
 
-const links = {
-  firstCol: [
-    { href: '/contributions', linkText: 'Contribute' },
-    { href: 'https://www.ibm.com/privacy', linkText: 'Privacy' },
-    { href: 'https://www.ibm.com/legal', linkText: 'Terms of use' },
-    { href: 'https://ibm.com', linkText: 'IBM.com' },
-  ],
+  /**
+   * Links for the left side of the Footer
+   * @type {{firstCol: *[]}}
+   */
+  const links = {
+    firstCol: [
+      { href: '/contributions', linkText: 'Contribute' },
+      { href: 'https://www.ibm.com/privacy', linkText: 'Privacy' },
+      { href: 'https://www.ibm.com/legal', linkText: 'Terms of use' },
+      { href: 'https://ibm.com', linkText: 'IBM.com' },
+      { href: '#', linkText: '' },
+    ],
 //  secondCol: [
 //    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
 //    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
 //    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
 //    { href: 'https://ibm.com/design', linkText: 'Shadowed link' },
 //  ],
-};
+  };
 
-const CustomFooter = () => <Footer links={links} Content={Content} />;
+  useEffect(() => {
+    // Appends the data-autoid for the last footer item
+    const footer = document.getElementsByTagName('footer');
+    const ul = footer[0].getElementsByTagName('ul');
+    const li = ul[0].getElementsByTagName('li');
+    const atag = li[li.length - 1].getElementsByTagName('a')[0];
+    atag.parentNode.removeChild(atag);
+    li[li.length - 1].setAttribute('data-autoid', 'dds--privacy-cp');
+  }, []);
+
+  return (
+    <Footer links={links} Content={Content} />
+  );
+};
 
 export default CustomFooter;
