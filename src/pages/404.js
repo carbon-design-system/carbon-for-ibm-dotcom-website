1633c769e7cc2177e43077f70632515ef11cf9d0
Jacob Ottesen <56400049+jacobottesen-dgc@users.noreply.github.com>
2020-04-24 13:14:42 -0500

    Update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { FourOhFour } from 'gatsby-theme-carbon';
 
 const links = [
-  { href: '/components/markdown', text: 'Markdown' },
-  { href: '/components/Aside', text: 'Aside' },
-  { href: '/components/demo', text: 'Demo' },
+  { href: '/chelp/index', text: 'Help' },
+  { href: 'https://ibm-studios.slack.com/archives/C2PLX8GQ6', text: 'Slack' },
+  { href: 'https://github.com/carbon-design-system/ibm-dotcom-library', text: 'Github repo' },
 ];
 
 const Custom404 = () => <FourOhFour links={links} />;
