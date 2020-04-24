5b13bdaf1b37d03ec3710f434c30908a3e90fc68
Jacob Ottesen <56400049+jacobottesen-dgc@users.noreply.github.com>
2020-04-24 13:30:53 -0500

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FourOhFour } from 'gatsby-theme-carbon';
 
 const links = [
-  { href: '/chelp/index', text: 'Help' },
+  { href: '/help/index', text: 'Help' },
   { href: 'https://ibm-studios.slack.com/archives/C2PLX8GQ6', text: 'Slack' },
   { href: 'https://github.com/carbon-design-system/ibm-dotcom-library', text: 'Github repo' },
 ];
