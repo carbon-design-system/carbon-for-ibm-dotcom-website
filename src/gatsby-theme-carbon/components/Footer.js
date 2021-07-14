e4240b5c4a3703f2b06a4710fe86f98351b0305f
Jeffrey Chew <jeff.chew@gmail.com>
2021-07-14 09:13:04 -0400

    docs(cdn): adding new/updated documentation on CDN packages (#1090)
    * docs(cdn): adding new/updated documentation on CDN packages
    
    * chore(footer): updating last updated date
    
    * Update src/pages/developing/frameworks/web-components.mdx
    
    Co-authored-by: Jan Child <44503588+janchild@users.noreply.github.com>
    
    * Update src/data/nav-items.yaml
    
    Co-authored-by: Jan Child <44503588+janchild@users.noreply.github.com>
    
    * Update src/pages/developing/carbon-cdn-style-helpers/heading.txt
    
    Co-authored-by: Jan Child <44503588+janchild@users.noreply.github.com>
    
    Co-authored-by: Jan Child <44503588+janchild@users.noreply.github.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- src/gatsby-theme-carbon/components/Footer.js
+++ src/gatsby-theme-carbon/components/Footer.js
@@ -21,7 +21,7 @@ const CustomFooter = () => {
         .
       </p>
       <p>
-        Last updated February 24th, 2021
+        Last updated July 13th, 2021
         <br />
         Copyright &copy; 2021 IBM
       </p>
