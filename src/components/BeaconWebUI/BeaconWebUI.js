2e40ea4a92282f69917eda9a5a58ee82aefacb48
kennylam <kennylam@protonmail.com>
2021-08-26 09:03:32 -0400

    feat(beacon): add beacon web ui page (#1155)
    * feat(beacon): add beacon web ui page
    
    * feat(beacon): add beacon web ui page
    
    * chore(docs): update beacon web ui page content
    
    * Update beacon.mdx
    
    @kennylam I jumped in and made some edits this morning to bring the audit field up on the page for return users. Also tried out a different page title, trying to keep Beacon in the name but also saying what it does because Beacon on its own hides the value unless you know what it does already. I thought `evaluator` over `checker` but we can see how people respond. 
    
    There's not really a way around the `What's included` being pushed down the page with the results. Bringing that content up above the URL field will push everything down too far and the `What's included` is mainly for first-time users, although this info will also be updated over time. The anchor links should help us out here. They're easy to scan and they'll  help people if they want to read before diving in.  
    
    Anyway, let's see how it looks!
    
    * Update beacon.mdx
    
    removing the link to the website from our team name
    
    Co-authored-by: Jan Child <44503588+janchild@users.noreply.github.com>

---
+++ src/components/BeaconWebUI/BeaconWebUI.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Button, Form, Loading, TextInput } from "carbon-components-react";
+import './beacon-web-ui.scss';
+
+/**
+ * Beacon web UI component
+ *
+ * @returns {*} Beacon web UI component
+ * @constructor
+ */
+export const BeaconWebUI = () => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  /* eslint-disable no-useless-escape */
+  const urlPattern = '^(http|https):\/\/[^ "]+$';
+
+  const formProps = {
+    className: 'beacon-web-ui__form',
+    onSubmit: (evt) => {
+      evt.preventDefault();
+      setIsLoading(true);
+      const iframe = document.getElementById('beacon-web-ui-iframe');
+      const url = document.getElementById('text-input-1').value;
+      fetch(`https://beacon-for-ibm-dotcom-api.herokuapp.com/?raw=true&url=${url}`)
+        .then(response => response.text())
+        .then(data => {
+          setIsLoading(false);
+          iframe.classList.add('has-results');
+          iframe.srcdoc = data;
+        });
+    }
+  };
+
+  return (
+    <>
+      <Form {...formProps}>
+        <TextInput
+          size="lg"
+          pattern={urlPattern}
+          required
+          id="text-input-1"
+          placeholder="Enter URL to audit, for example https://www.ibm.com"
+        />
+        <Button type="submit">Analyze page</Button>
+      </Form>
+      <Loading active={isLoading} />
+      <iframe id="beacon-web-ui-iframe" title="Beacon for IBM.com" src=""></iframe>
+    </>
+  );
+}
