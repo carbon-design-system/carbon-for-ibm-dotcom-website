30f8c63981eba92a79a74a54369dd4dac428abf3
Olivia Flory <45692622+oliviaflory@users.noreply.github.com>
2023-09-13 16:23:17 -0400

    update homepage to white

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
           subDirectory: '',
         },
         theme: {
-          homepage: 'dark',
+          homepage: 'white',
           interior: 'g10',
         },
       },
